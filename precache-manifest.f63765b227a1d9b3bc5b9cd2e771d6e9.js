13f38b8dff40513f1ed87cf39389cffa5e8d4a1b
Josh Gude <jgude@Joshs-MacBook-Pro.local>
2018-10-09 00:45:10 -0400

    Updates

---
+++ precache-manifest.f63765b227a1d9b3bc5b9cd2e771d6e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "08159512fb0d87effc4f",
+    "url": "/Budgetor/static/js/runtime~main.08159512.js"
+  },
+  {
+    "revision": "ad64c54926652613f901",
+    "url": "/Budgetor/static/js/main.ad64c549.chunk.js"
+  },
+  {
+    "revision": "1cc322f13b547160124f",
+    "url": "/Budgetor/static/js/1.1cc322f1.chunk.js"
+  },
+  {
+    "revision": "ad64c54926652613f901",
+    "url": "/Budgetor/static/css/main.43ace0ec.chunk.css"
+  },
+  {
+    "revision": "06595b0569e0efc8e537722151176a45",
+    "url": "/Budgetor/index.html"
+  }
+];
